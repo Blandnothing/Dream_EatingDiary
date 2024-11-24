a95c0d013e0e0d0ce8a12300ff03647b43db267a
Blandnothing <2067721994@qq.com>
2024-11-25 06:42:05 +0800

    更新资源框架

--- Assets/Scripts/Avatar/BaseClass/Resource.cs
+++ Assets/Scripts/Avatar/BaseClass/Resource.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//资源大类
+public enum ResourceCategory
+{
+    Null,
+    Nightmare,        //梦魇自身持有的资源，即饥饿度和意识开放度
+    Mine,             //矿物，从关卡内获得的基础资源
+    Item              //道具，
+}
+//资源小类
 public enum ResourceType{
+    Null,
+    
+    //Nightmare
+    Hunger,         //饥饿度
+    Openness,       //意识开放度
+    
+    //Mine
+    Gold,
+    Silver,
+    Copper,
+    Gem,
+    
+    //Item
     
 }
 
-public class Resource 
-{
-    
-}
