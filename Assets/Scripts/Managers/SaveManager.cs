b4e9eb85b40e0f952082c4589f7e709f845e903b
Blandnothing <2067721994@qq.com>
2024-11-30 21:46:14 +0800

    添加存档系统

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveManager : Singleton<SaveManager> 
-{   
-    
+{
+    class SavaData
+    {
+        public Dictionary<ResourceType, int> resourceCount;
+    }
+    SavaData data;
+
+    public SaveManager()
+    {
+        
+    }
+
+    void ReadDate()
+    {
+        if(!File.Exists(Application.persistentDataPath))
+        {
+            System.IO.Directory.CreateDirectory(Application.persistentDataPath);
+        }
+        string path = Application.persistentDataPath + "/saveData.json";
+        if (!File.Exists(path))
+        {
+            File.Create(path).Close();
+        }
+    }
 }
